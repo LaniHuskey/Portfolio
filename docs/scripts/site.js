6058ac01f634633ae50772496ca0b02baa92c053
Andreas Feldstedt <afeldstedt@gmail.com>
2022-09-06 14:53:45 +0200

    Added JavaScript

---
+++ docs/scripts/site.js
@@ -0,0 +1,47 @@
+﻿var details = $('.detail-container');
+var currentId = "";
+details.children().hide();
+
+function loadDetails(id) {
+    scrollToHeader('portfolio');
+
+    if (id !== currentId) {
+        currentId = id;
+        clearTimeout(slideTimeout);
+
+        details.children().hide();
+        $('#' + id).show();
+        playSlideshow(id);
+    }
+}
+
+// Slideshow
+var slideIndex = 0;
+var slides;
+var slideTimeout;
+
+function playSlideshow(id) {
+    slides = $('#' + id).children('.slideshow').children('.slide');
+
+    changeSlide();
+}
+
+function changeSlide() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slideIndex++;
+
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+
+    slides[slideIndex].style.display = 'block';
+    slideTimeout = setTimeout(changeSlide, 4000);
+}
+
+// Navigation
+function scrollToHeader(id) {
+    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
